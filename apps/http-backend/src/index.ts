1ba7081a17deaab54b53cb63a3fae79726ed7fc8
naushad91 <naushadkhank426@gmail.com>
2025-07-12 17:31:50 +0530

    add a common package to all fe and fe that is zod schemas

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,14 +3,39 @@ import jwt from "jsonwebtoken";
 
 import {JWT_SECRET} from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import { CreateRoomSchema, SigninSchema,CreateUserSchema } from "@repo/common/types";
 
 const app = express();
 
-app.post("/signup", (req, res) => {
-  // signup logic goes here
+
+  app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs"
+    });
+    return;
+  }
+
+  // db call
+  res.json({
+    userId: "123"
+  });
+
+
 });
 
 app.post("/signin", (req, res) => {
+
+    const data = SigninSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs"
+    });
+    return;
+  }  
   const userId = 1;
   const token=jwt.sign(
     {
@@ -24,7 +49,18 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/room", middleware,(req, res) => {
+      const data = CreateRoomSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs"
+    });
+    return;
+  }
   // room logic goes here
+  res.json({
+    roomId: 123
+  })
 });
 
 app.listen(3001);
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
+import { CreateRoomSchema, SigninSchema, CreateUserSchema } from "@repo/common/types";
 
 export function middleware(req: Request, res: Response, next: NextFunction) {
   const token = req.headers["authorization"] ?? "";
