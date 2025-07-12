e731bc2c5a3ff685bc0a46e33fd4d1e8f80e9b41
naushad91 <naushadkhank426@gmail.com>
2025-07-12 15:49:12 +0530

    added http frontend logic with gatted middleware and ws-logic

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,29 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
-const app=express();
+const app = express();
+
+app.post("/signup", (req, res) => {
+  // signup logic goes here
+});
+
+app.post("/signin", (req, res) => {
+  const userId = 1;
+  const token=jwt.sign(
+    {
+      userId
+    },
+    JWT_SECRET
+  );
+  res.json({
+    token
+  })
+});
+
+app.post("/room", middleware,(req, res) => {
+  // room logic goes here
+});
 
 app.listen(3001);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,25 @@
 import { WebSocketServer } from 'ws';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from './config';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request) {
+  const url = request.url; // ws://localhost:3000?token=123123
+  // ["ws://localhost:3000", "token=123123"]
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split('?')[1]);
+  const token = queryParams.get('token') || '';
+  const decoded = jwt.verify(token, JWT_SECRET);
+//@ts-ignore
+  if (!decoded || !decoded.userId) {
+    ws.close();
+    return;
+  }
+
   ws.on('message', function message(data) {
     ws.send('pong');
   });
