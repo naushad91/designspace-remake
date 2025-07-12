547f456593c2ccccb769a91a71a496df6597a6ef
naushad91 <naushadkhank426@gmail.com>
2025-07-12 16:24:14 +0530

    add a common backend package for jwt secret parsing

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,6 @@
 import { WebSocketServer } from 'ws';
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from './config';
+import {JWT_SECRET} from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -15,7 +15,7 @@ wss.on('connection', function connection(ws, request) {
   const token = queryParams.get('token') || '';
   const decoded = jwt.verify(token, JWT_SECRET);
 //@ts-ignore
-  if (!decoded || !decoded.userId) {
+  if (!decoded || !(decoded as JwtPayload).userId) {
     ws.close();
     return;
   }
