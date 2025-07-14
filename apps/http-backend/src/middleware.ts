2a4942273db203b081963d88890b11e594a03adc
naushad91 <naushadkhank426@gmail.com>
2025-07-14 13:21:53 +0530

    added full websocket and https server logic

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,20 +1,20 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
-import { CreateRoomSchema, SigninSchema, CreateUserSchema } from "@repo/common/types";
+
 
 export function middleware(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers["authorization"] ?? "";
+    const token = req.headers["authorization"] ?? "";
 
-  const decoded = jwt.verify(token, JWT_SECRET);
+    const decoded = jwt.verify(token, JWT_SECRET);
 
-  if (decoded) {
-   // @ts-ignore
-    req.userId = decoded.userId;
-    next();
-  } else {
-    res.status(403).json({
-      message: "Unauthorized"
-    });
-  }
+    if (decoded) {
+        // @ts-ignore: TODO: Fix this
+        req.userId = decoded.userId;
+        next();
+    } else {
+        res.status(403).json({
+            message: "Unauthorized"
+        })
+    }
 }
